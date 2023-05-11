1c13f67392342acb93d4a2435d8fd8bb5d7b4b14
phuhoangdev <lhp1996nt@gmail.com>
2023-05-11 22:08:40 +0700

    Code splitting

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import { Route, Routes } from 'react-router-dom';
-import 'swiper/scss';
-import Main from './components/layout/Main';
-import HomePage from './pages/HomePage';
-import Banner from './components/banner/Banner';
-import MoviePage from './pages/MoviePage';
-import MovieDetailsPage from './pages/MovieDetailsPage';
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import "swiper/scss";
+import Main from "./components/layout/Main";
+import Banner from "./components/banner/Banner";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
 
 const App = () => {
-   return (
-      <>
-         <Routes>
-            <Route element={<Main />}>
-               <Route
-                  path="/"
-                  element={
-                     <>
-                        <Banner />
-                        <HomePage />
-                     </>
-                  }
-               />
-               <Route
-                  path="/movies"
-                  element={
-                     <>
-                        <MoviePage />
-                     </>
-                  }
-               />
-               <Route
-                  path="/movie/:movieId"
-                  element={
-                     <>
-                        <MovieDetailsPage />
-                     </>
-                  }
-               />
-            </Route>
-         </Routes>
-      </>
-   );
+     return (
+          <>
+               <Suspense fallback={<></>}>
+                    <Routes>
+                         <Route element={<Main />}>
+                              <Route
+                                   path="/"
+                                   element={
+                                        <>
+                                             <Banner />
+                                             <HomePage />
+                                        </>
+                                   }
+                              />
+                              <Route
+                                   path="/movies"
+                                   element={
+                                        <>
+                                             <MoviePage />
+                                        </>
+                                   }
+                              />
+                              <Route
+                                   path="/movie/:movieId"
+                                   element={
+                                        <>
+                                             <MovieDetailsPage />
+                                        </>
+                                   }
+                              />
+                         </Route>
+                    </Routes>
+               </Suspense>
+          </>
+     );
 };
 
 export default App;
